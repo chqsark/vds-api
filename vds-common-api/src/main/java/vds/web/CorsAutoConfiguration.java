6cc8c832d11243bcaf9138ca9b0954a4bfbfdce6
Kuisong Tong <kuisong.tong@gmail.com>
2014-07-30 20:33:33 -0700

    remove dependency of Jetty

--- vds-common-api/src/main/java/vds/web/CorsAutoConfiguration.java
+++ vds-common-api/src/main/java/vds/web/CorsAutoConfiguration.java
@@ -1,29 +1,29 @@
 package vds.web;
 
 import org.apache.catalina.filters.CorsFilter;
-import org.apache.catalina.startup.Tomcat;
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
-import org.eclipse.jetty.util.Loader;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.condition.SearchStrategy;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 import javax.servlet.Filter;
 import javax.servlet.Servlet;
 
 @Configuration
 @ConditionalOnWebApplication
+@Order(Ordered.HIGHEST_PRECEDENCE + 10)
+@AutoConfigureAfter(EmbeddedServletContainerAutoConfiguration.class)
 public class CorsAutoConfiguration {
     /**
      * Nested configuration for if Tomcat is being used.
      */
     @Configuration
-    @ConditionalOnClass({Servlet.class, Tomcat.class})
+    @ConditionalOnClass({Servlet.class, CorsFilter.class})
     public static class CorsTomcat {
         @Bean
         public Filter corsFilter() {
@@ -35,12 +35,10 @@ public class CorsAutoConfiguration {
      * Nested configuration if Jetty is being used.
      */
     @Configuration
-    @ConditionalOnClass({Servlet.class, Server.class, Loader.class, CrossOriginFilter.class})
-    @ConditionalOnMissingBean(value = EmbeddedServletContainerFactory.class, search = SearchStrategy.CURRENT)
+    @ConditionalOnClass({Servlet.class, CrossOriginFilter.class})
     public static class CorsJetty {
         @Bean
         public Filter corsFilter() {
-            System.out.print("initial jetty cors");
             return new CrossOriginFilter();
         }
     }
