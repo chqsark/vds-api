483324b5c6a1154254e6f38889618f0264ecf251
Kuisong Tong <kuisong.tong@gmail.com>
2014-08-07 22:46:45 -0700

    add WebSocket for tracking dom and events

---
+++ vds-track-api/src/main/java/vds/track/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package vds.track.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import vds.track.api.TrackWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(trackWebSocketHandler(), "/track").withSockJS();
+    }
+
+    @Bean
+    public WebSocketHandler trackWebSocketHandler() {
+        return new TrackWebSocketHandler();
+    }
+}
