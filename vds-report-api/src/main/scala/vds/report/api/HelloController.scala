b2fcec8f453dd828a59a89c6f6b1a4abd4100083
Kuisong Tong <kuisong.tong@gmail.com>
2014-08-21 15:59:17 -0700

    switch report to scala

---
+++ vds-report-api/src/main/scala/vds/report/api/HelloController.scala
@@ -0,0 +1,11 @@
+package vds.report.api
+
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+
+@RestController
+class HelloController {
+  @RequestMapping(Array("/"))
+  def index() = {
+    "Greetings from Spring Boot!"
+  }
+}
