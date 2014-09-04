31c3f3c7bade50aa1667f9628c1eb21a827682c9
Kuisong Tong <kuisong.tong@gmail.com>
2014-09-03 21:49:14 -0700

    add counter for tracking messages

--- vds-track-api/src/main/java/vds/track/api/TrackController.java
+++ vds-track-api/src/main/java/vds/track/api/TrackController.java
@@ -1,24 +1,26 @@
 package vds.track.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackController {
-    private static Logger logger = LoggerFactory.getLogger(TrackController.class);
+    private final CounterService counterService;
     private final Tracker tracker;
 
     @Autowired
-    public TrackController(Tracker tracker) {
+    public TrackController(CounterService counterService, Tracker tracker) {
+        this.counterService = counterService;
         this.tracker = tracker;
     }
 
     @MessageMapping("/{id}")
     public void track(@DestinationVariable("id") String id, String message) throws Exception {
+        counterService.increment("counter.tracker.received");
         tracker.track(id, message);
+        counterService.increment("counter.tracker.sent");
     }
 }
