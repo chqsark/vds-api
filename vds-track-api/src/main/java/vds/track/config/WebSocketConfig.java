15e80905fe764a54327703fe73522a5b83f334a7
Kuisong Tong <kuisong.tong@gmail.com>
2014-09-10 22:36:32 -0700

    set limit of message size

--- vds-track-api/src/main/java/vds/track/config/WebSocketConfig.java
+++ vds-track-api/src/main/java/vds/track/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package vds.track.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.ByteArrayMessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
@@ -8,12 +9,21 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+    @Value("${message.size.limit:1048576}")
+    private int messageSizeLimit;
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(messageSizeLimit);
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
